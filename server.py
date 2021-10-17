c5bf1af3b1a34c6be817f67c958a7abcf5c76728
DeWittDev <88995967+DeWittDev@users.noreply.github.com>
2021-10-17 12:32:39 -0700

    10/17

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, redirect, request, session
 app = Flask(__name__)
-app.secret_key - "Standing Man"
+app.secret_key = "Standing Man"
 
 @app.route('/')
 def index():
-    return render_template(index.html)
+    return render_template('index.html')
+
+@app.route('/results')
+def results():
+    return render_template('result.html')
 
 if __name__=='__main__':
     app.run(debug=True)
